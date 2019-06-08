0381926ef55cb60ad712018d543af21fd68fa631
Dayvid Moreira <dayvid_tjk@hotmail.com>
2019-06-08 14:26:10 -0300

    habilitando rotas de usuarios no index

--- src/index.js
+++ src/index.js
@@ -18,23 +18,23 @@ app.get('/', (req, res) => {
     res.send(`Main`)
 });
 
-// app.get('/users', async (req, res) => {
-//     try {
-//         let users = await User.find();
-//         return res.send(users);
-//     } catch (error) {
-//         return res.status(400).send(error);
-//     };
-// });
+app.get('/users', async (req, res) => {
+    try {
+        let users = await User.find();
+        return res.send(users);
+    } catch (error) {
+        return res.status(400).send(error);
+    };
+});
 
-// app.delete('/users/:userID', async(req, res) => {
-//     try {
-//         await User.findOneAndRemove({_id: req.params.userID});
-//         return res.send('Foi');
-//     } catch (error) {
-//         return res.status(400).send(error);
-//     }
-// })
+app.delete('/users/:userID', async(req, res) => {
+    try {
+        await User.findOneAndRemove({_id: req.params.userID});
+        return res.send('Foi');
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Listening');
