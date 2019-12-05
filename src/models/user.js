d527813ce9e414cc0da4245b2371a37f3638dd4c
Luiz <lf1405@hotmail.com>
2019-12-05 16:27:26 -0300

    Senha oculta

--- src/models/user.js
+++ src/models/user.js
@@ -18,7 +18,8 @@ const userSchema = new Schema(
     },
     password: {
       type: String,
-      required: true
+      required: true,
+      select: false
     },
     message: {
       type: String
