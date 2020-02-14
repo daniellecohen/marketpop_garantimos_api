b6f1e8a568805fc7cdad15d8a60af93769f12ff1
Luiz <lf1405@hotmail.com>
2020-02-13 23:49:55 -0300

    utilizando banco certo no servidor de testes

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
   console.log("Database is connected");
 });
 mongoose.set("useFindAndModify", false);
