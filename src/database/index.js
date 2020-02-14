f386983bfc16b77ea5be46b5fcf796accd3b454c
Luiz <lf1405@hotmail.com>
2020-02-14 00:13:13 -0300

    vendo URI do mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-
+console.log("mongo_URI", process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
   console.log("Database is connected");
 });
